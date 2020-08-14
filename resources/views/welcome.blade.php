eecccb9cc062a326459f38d4486e8649b52bba48
mohamedhosni <mohamed.hosni3202@gmail.com>
2020-08-14 22:50:03 +0200

    change welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Pos System
                 </div>
 
                 <div class="links">

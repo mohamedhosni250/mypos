df2e63e35f8cd0070f9d20a336cc6e167181822c
mohamedhosni <mohamed.hosni3202@gmail.com>
2020-08-14 22:34:55 +0200

    web edit file

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,5 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/about', 'HomeController@index')->name('home');
+Route::get('/us', 'HomeController@index')->name('home');

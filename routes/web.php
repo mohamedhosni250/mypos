89a01574b8a08767cd8ba5f5b980758395f2b93c
mohamedhosni <mohamed.hosni3202@gmail.com>
2020-08-21 15:50:01 +0200

    adding language mcamara and lang switcher

--- routes/web.php
+++ routes/web.php
@@ -13,23 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-<<<<<<< HEAD
-Route::group(['prefix' => LaravelLocalization::setLocale()], function()
-{
+Route::group(['prefix' => LaravelLocalization::setLocale()], function() {
     /** ADD ALL LOCALIZED ROUTES INSIDE THIS GROUP **/
+
+
     Route::get('/', function () {
         return view('dashboard.index');
-    })->middleware('auth');
-
-=======
-Route::get('/', function () {
-    return view('dashboard.index');
->>>>>>> 5f9f7a55ae46dccc2d66406826e376cb6bb4cbf5
+    });
 });
-
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/about', 'HomeController@index')->name('home');
-Route::get('/updatelatest', 'HomeController@index')->name('home');
